8a6e5a899f3d52dcedd315cb48c56c08d3d9c58d
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-03-01 08:50:15 -0600

    Probar FirstApp

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -6,7 +6,22 @@ describe( 'Pruebas en <FirstApp/>', () => {
   test(' debe hacer match con el snapshot', () =>{
 
     const title = 'Hola, Soy Goku';
-    render( <FirstApp title= { title } /> );
+    const { container } =  render( <FirstApp title= { title } /> );
+
+    expect( container ).toMatchSnapshot();
+
+  });
+
+  test('debe de mostrar el títilo en un h1', () => {
+
+    const title = 'Hola, Soy Goku';
+    const { container, getByText } =  render( <FirstApp title= { title } /> );
+
+    expect( getByText(title) ).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+    // console.log(h1.innerHTML);
+    expect(h1.innerHTML).toContain( title );
 
   });
 
