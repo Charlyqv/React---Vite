619df66d8cba6b5d903991b1668fdbcae2211815
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-03-06 09:50:26 -0600

    Simular eventos - Click

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe(' Pruebas den <CounterApp/>', () => {
 
-  const counter = 1001;
+  const counter = 10;
 
   test(' debe de hacer match con el snapshot', () => {
     const { container } = render( <CounterApp counter = { counter } /> );
@@ -28,6 +28,34 @@ describe(' Pruebas den <CounterApp/>', () => {
     
     render( <CounterApp value={ 100 } /> );
     expect( screen.getByText(100)).toBeTruthy();
+
+  });
+
+  test('debe de incrementar con el botón +1', () => {
+
+    render( <CounterApp value={ counter } /> );
+    fireEvent.click( screen.getByText('+1') )
+    expect( screen.getByText('11') ).toBeTruthy();
+
+  });
+
+  test('debe de decrementar con el botón -1', () => {
+
+    render( <CounterApp value={ counter } /> );
+    fireEvent.click( screen.getByText('-1') )
+    expect( screen.getByText('9') ).toBeTruthy();
+
+  });
+
+  test('debe dee funcionar el botón de reset', () => {
+
+    render( <CounterApp value={ 355 } /> );
+    fireEvent.click( screen.getByText('+1') );
+    fireEvent.click( screen.getByText('+1') );
+    // fireEvent.click( screen.getByText('Reset') );
+    fireEvent.click(screen.getByTestId('button', { id: 'btn-reset'}));
+
+    expect( screen.getByText( 355 ) ).toBeTruthy();
 
   });
 });
